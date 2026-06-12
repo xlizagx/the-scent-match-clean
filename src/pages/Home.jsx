e57f1491b75036e5f6b801463b9371cb06a20cf9
xlizagx <lizageorgiou@hotmail.com>
2026-06-12 23:39:59 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HeroSection from '../components/home/HeroSection';
+import HeroWithMatches from '../components/home/HeroWithMatches';
 import TrustSection from '../components/home/TrustSection';
 import MemorySection from '../components/home/MemorySection';
 import PathsSection from '../components/home/PathsSection';
@@ -10,12 +10,11 @@ import RecentMatches from '../components/home/RecentMatches';
 
 const HERO_IMAGE = 'https://media.base44.com/images/public/69cac3f60d3002bf060b0af7/6f6a5c1f7_generated_image.png';
 const MEMORY_IMAGE = 'https://media.base44.com/images/public/69cac3f60d3002bf060b0af7/e7784ae5d_generated_5faabd6f.png';
-const GIFT_IMAGE = 'https://media.base44.com/images/public/69cac3f60d3002bf060b0af7/09d8eb581_generated_1e6e2cb0.png';
 
 export default function Home() {
   return (
     <div>
-      <HeroSection heroImage={HERO_IMAGE} />
+      <HeroWithMatches heroImage={HERO_IMAGE} />
       <RecentMatches />
       <TestimonialsSection />
       <TrustSection />
