f83b6bf44e5d5ed3e750b00181d4d06952a9dde0
xlizagx <lizageorgiou@hotmail.com>
2026-09-16 21:10:39 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import HeroSection from '../components/home/HeroSection';
-import TrustSection from '../components/home/TrustSection';
-import MemorySection from '../components/home/MemorySection';
-import PathsSection from '../components/home/PathsSection';
-import TestimonialsSection from '../components/home/TestimonialsSection';
-import ConsultationTeaser from '../components/home/ConsultationTeaser';
 import FooterCTA from '../components/home/FooterCTA';
-import RecentMatches from '../components/home/RecentMatches';
 
 const HERO_IMAGE = '/hero-hoodie.png';
-const MEMORY_IMAGE = 'https://media.base44.com/images/public/69cac3f60d3002bf060b0af7/e7784ae5d_generated_5faabd6f.png';
 
 export default function Home() {
   return (
     <div>
       <HeroSection heroImage={HERO_IMAGE} />
-      <RecentMatches />
-      <TestimonialsSection />
-      <TrustSection />
-      <PathsSection />
-      <ConsultationTeaser />
-      <MemorySection memoryImage={MEMORY_IMAGE} />
       <FooterCTA />
     </div>
   );
